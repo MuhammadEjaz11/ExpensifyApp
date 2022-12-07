f70525edef7c1e54f118c26577a0046bbf952161
MuhammadEjaz11 <mejaz4349@gmail.com>
2022-12-07 16:32:18 +0500

    set-up_expensify_app

---
+++ app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Style/style.scss'
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<p>This is expensify app</p>, document.getElementById("app"))
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+console.log(__dirname)
+const path = require('path');
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+
+        },
+        {
+            test: /\.scss$/,
+            use:[
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }],
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        allowedHosts: 'all',
+        static: {
+            directory: __dirname,
+        },
+    },
+    mode: 'development',
+
+}
