7152ac202651dd596f8699d4686534387284d707
Muhammad Ejaz <mejaz4349@gmail.com>
2022-12-10 03:50:10 +0500

    class 2 router

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Style/style.scss'
-import {BrowserRouter,Route, Routes} from 'react-router-dom';
+import {BrowserRouter,Route, Routes, Link, NavLink} from 'react-router-dom';
+
+const Header = ()=> (
+    <div> 
+        <h1>Header</h1> <br/>
+        <NavLink to='/' activeClassName='active' >Go to Dashboard </NavLink> <br/>
+        <NavLink to='/add'>Go to AddExpense </NavLink> <br/>
+        <NavLink to='/edit'>Go to EditExpense </NavLink> <br/>
+        <NavLink to='/help'>Go to Help </NavLink> <br/>
+    </div>
+);
 
 const ExpenseDashboard = ()=> (
     <div>
         This is Expense Dashboard component
+        
+        
     </div>
 );
 
@@ -24,17 +36,28 @@ const Help = ()=> (
         This is Help component
     </div>
 );
+const NotFound = ()=> (
+    <div>
+        404!
+        <Link to='/'>Go to Home </Link> <br/>
+    </div>
+);
 
 const routes = (
     <BrowserRouter>
-    <div>
+        <div>
+            <Header/>
+        </div>
         <Routes>
-        <Route path='/' component={ExpenseDashboard} exact={true}/>
+        <Route path='/' element={<ExpenseDashboard/>}/>
+        <Route path='/add' element={<AddExpense/>}/>
+        <Route path='/edit' element={<EditExpense/>}/>
+        <Route path='/help' element={<Help/>}/>
+        <Route path='*' element={<NotFound/>}/>
 
 
         </Routes>
 
-    </div>
     </BrowserRouter>
 )
 
