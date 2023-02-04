e46f22f20bd0e2b7a1bcc5d7ff77873e2149bed1
MuhammadEjaz11 <mejaz4349@gmail.com>
2023-02-04 05:26:34 +0500

    done with add expenses

--- Actions/expense.js
+++ Actions/expense.js
@@ -1,6 +1,8 @@
 import {v4 as uuidv4} from 'uuid';
 import {app, database} from '../Firebase/firebase';
 import { ref, remove, set, update,get,child,onValue } from "firebase/database";
+import moment from'moment';
+import expense from '../Reducers/expense';
 
 
 export const Add_Expense = (expense)=>({
@@ -17,10 +19,17 @@ export const startAddExpense = (expenseData  = {})=>{
             note = '',
             amount = 0,
             createDate = 0,
-            time= 0
+            time=0
         } = expenseData;
-        const expense = {id,description, note,amount,createDate,time};
-        set(ref(database,'expense'),{id,description,note,amount,time}).then(()=>{
+        const expense = {
+            id,
+            description,
+            note,
+            amount,
+            createDate:moment(createDate._d).format("YYYY-MM-DD")};
+        
+        console.log(createDate._d)
+        set(ref(database,'expense/'+expense.id),{description, note,amount,createDate}).then(()=>{
             dispatch(Add_Expense({
                 ...expense
             }))
