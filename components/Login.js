99e742721e42551a482740dd28fbad1d24a9ec52
Muhammad Ejaz <mejaz4349@gmail.com>
2023-03-22 05:25:05 +0500

    ss

--- components/Login.js
+++ components/Login.js
@@ -5,19 +5,23 @@ import Header from "./Header";
 
 
 
-const Login  = ({StartLogin})=>(
+const Login = ({ StartLogin }) => (
 
-    <div>
-        <button onClick={StartLogin}>Login</button>
+    <div className="box-layout">
+        <div className="main-box">
+            <h1 className="main-box__title">Expensify App</h1>
+            <p className="main-box__sub-title">It's time to get your expenses undercontrol</p>
+            <button onClick={StartLogin}>Login</button>
+        </div>
 
     </div>
 );
 
-const mapDispatchToProps = (dispatch)=>(
+const mapDispatchToProps = (dispatch) => (
     {
-        StartLogin: ()=> dispatch(StartLogin())
+        StartLogin: () => dispatch(StartLogin())
     }
 );
 
 
-export default connect(undefined,mapDispatchToProps)(Login);
+export default connect(undefined, mapDispatchToProps)(Login);
