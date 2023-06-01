58ce6d5e63e0ea7ec723aaaa6d25ae868878cfda
Muhammad Ejaz <mejaz4349@gmail.com>
2023-06-01 06:11:06 +0500

    everything is done, just waiting for final deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,108 @@
-console.log(__dirname)
+// console.log(__dirname)
+// const path = require('path');
+
+// module.exports = {
+//     entry: './app.js',
+//     output: {
+//         path: __dirname,
+//         filename: "bundle.js"
+//     },
+//     module: {
+//         rules: [{
+//             test: /\.js$/,
+//             loader: 'babel-loader',
+//             exclude: /node_modules/,
+
+//         },
+//         {
+//             test: /\.scss$/,
+//             use:[
+//                 'style-loader',
+//                 'css-loader',
+//                 'sass-loader'
+//             ]
+//         },
+//         {
+//             test: /\.css$/,
+//             use:[
+//                 'style-loader',
+//                 'css-loader',
+//             ]
+//         }
+//     ],
+//     },
+//     devtool: 'eval-cheap-module-source-map',
+//     devServer: {
+//         allowedHosts: 'all',
+//         static: {
+//             directory: __dirname,
+//         },
+//         historyApiFallback: true
+//     },
+//     mode: 'development',
+
+// }
+
+
+
+
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './app.js',
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
 
+    return {
+        entry: './app.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: isDevelopment ? '[name].bundle.js' : '[name].[contenthash].bundle.js',
+            publicPath: '/'
         },
-        {
-            test: /\.scss$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                    ]
+                }
+
             ]
         },
-        {
-            test: /\.css$/,
-            use:[
-                'style-loader',
-                'css-loader',
-            ]
-        }
-    ],
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        allowedHosts: 'all',
-        static: {
-            directory: __dirname,
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html'
+            })
+        ].filter(Boolean),
+        devServer: {
+            historyApiFallback: true,
+            //   contentBase: './dist',
+            hot: true
         },
-        historyApiFallback: true
-    },
-    mode: 'development',
-
-}
+        devtool: isDevelopment ? 'eval-source-map' : 'source-map'
+    };
+};
